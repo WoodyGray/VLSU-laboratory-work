be7da21ab3780f706ec90f6a90e086824be34b8a
WoodyGray <115577474+WoodyGray@users.noreply.github.com>
2023-10-25 11:21:27 +0300

    commit

--- optimization-methods/firstLabPython/calculation.py
+++ optimization-methods/firstLabPython/calculation.py
@@ -1,10 +1,95 @@
 from math import gcd
 from functools import reduce
 
-INF = 2 * 10**9
+INF = float('inf')
 
+# from math import gcd
+# from functools import reduce
+# INF = float('inf')
 
-def F(c, w, Y, a=[], b=[]):
+# def F(c, w, Y, a=None, b=None):
+#     if a is None:
+#         a = [0] * len(c)
+#     if b is None:
+#         b = [INF] * len(c)
+#
+#     step = reduce(gcd, w)
+#     f = [0] * len(c)
+#     szi_list = [0] * len(c)
+#
+#     for i in range(len(c)):
+#         max_val = min(Y // w[i], b[i])
+#
+#         f[i] = [0] * (Y // step + 1)
+#         szi_list[i] = [(0,) * len(c)] * (Y // step + 1)
+#
+#         if i == 0:
+#             for j in range(Y // step + 1):
+#                 cur_f = 0
+#                 cur_val = max(0, a[i])
+#                 opt_val = (cur_val,) + (0,) * (len(c) - 1)
+#                 while (w[i] * cur_val <= j * step) and (cur_val <= max_val):
+#                     cur_f = max(cur_f, c[i] * cur_val)
+#                     if cur_f < c[i] * cur_val:
+#                         cur_f = c[i] * cur_val
+#                         opt_val = (cur_val,) + (0,) * (len(c) - 1)
+#                     cur_val += 1
+#                 f[i][j] = cur_f
+#                 szi_list[i][j] = opt_val
+#
+#         if i > 0:
+#             for j in range(Y // step + 1):
+#                 cur_f = 0
+#                 cur_val = max(0, a[i])
+#                 opt_val = (cur_val,) + (0,) * (len(c) - 1)
+#                 surplus = j * step - w[i] * cur_val
+#
+#                 while (surplus >= 0) and (cur_val <= max_val):
+#                     if cur_f < c[i] * cur_val + f[i - 1][surplus // step]:
+#                         cur_f = c[i] * cur_val + f[i - 1][surplus // step]
+#                         opt_val = tuple(szi_list[i - 1][surplus // step][k] + (1 if k == i else 0) for k in range(len(c)))
+#                     cur_val += 1
+#                     surplus = j * step - w[i] * cur_val
+#                 f[i][j] = cur_f
+#                 szi_list[i][j] = opt_val
+#
+#     return [f[len(c) - 1][-1], szi_list[len(c) - 1][-1]]
+
+# def find_most_efficient_resources(F, resources):
+#     resources = sorted(resources, key=lambda x: x[2] / x[1], reverse=True)  # Сортируем по эффективности
+#     result = []
+#     remaining_cost = F
+#
+#     for res in resources:
+#         if remaining_cost == 0:
+#             break
+#         if remaining_cost >= res[2]:
+#             count = remaining_cost // res[2]
+#             result.append([res[0], count])
+#             remaining_cost -= count * res[2]
+#
+#     return result
+
+def find_most_efficient_resources(F, resources):
+    resources = sorted(resources, key=lambda x: x[2] / x[1], reverse=True)  # Сортируем по эффективности
+    result = []
+    remaining_cost = F
+
+    for res in resources:
+        count = 0
+        while remaining_cost >= res[2]:
+            count += 1
+            remaining_cost -= res[2]
+        result.append([res[0], count])
+
+    result.sort()
+    secondResult = []
+
+    for r in result:
+        secondResult.append(r[1])
+    return secondResult
+
+def F(c, w, Y,index, a=[], b=[]):
     if a == []:
         a = [0] * len(c)
     if b == []:
@@ -49,14 +134,26 @@ def F(c, w, Y, a=[], b=[]):
                     cur_val += 1
                     surplus = j * step - w[i] * cur_val
                 f[i][j] = cur_f
+
                 szi_list[i][j] = opt_val
 
-    return [f[len(c) - 1][-1], szi_list[len(c) - 1][-1]]
+    # return [f[len(c) - 1][-1], szi_list[len(c) - 1][-1]]
+    recourses = create(index, w, c)
+    return [f[len(c) - 1][-1], find_most_efficient_resources(f[len(c) - 1][-1], recourses)]
+
+def create(i, w, c):
+    result = []
+    for j in i:
+        result.append([i[j-1], w[j-1], c[j-1]])
+
+    return result
 
 #первая задача
 def firstTask(task = 1):
     c = [22, 26, 28, 33]
     w = [6, 7, 8, 10]
+    i = [1, 2, 3, 4]
+
     w1 = [x - 1 for x in w]
     w2 = [x - 2 for x in w]
     Y_start = 21
@@ -64,18 +161,23 @@ def firstTask(task = 1):
 
     if task == 2:
         print("Y ", "F1 ", "                    F2 ", "                     F3 ")
+
         for Y in range(Y_start, Y_end + 1):
-            print(Y, F(c, w, Y, a=[], b=[])[0], F(c, w1, Y, a=[], b=[])[0], F(c, w2, Y, a=[], b=[])[0])
+            print(Y, F(c, w, Y,i, a=[], b=[]), F(c, w1, Y,i, a=[], b=[]), F(c, w2, Y,i, a=[], b=[]))
+            # print(Y, F(c, w, Y, a=[], b=[])[0], F(c, w1, Y, a=[], b=[])[0], F(c, w2, Y, a=[], b=[])[0])
+            # print(Y, F(c, w, Y), F(c, w1, Y), F(c, w2, Y))
     elif task == 1:
         print("Y", "F", "Список СрЗИ")
         for Y in range(Y_start, Y_end + 1):
-            print(Y, *F(c, w, Y, a=[], b=[]))
+            print(Y, F(c, w, Y,i, a=[], b=[]))
+            # print(Y, *F(c, w, Y))
 
 
 #вторая задача
 def secondTask(task = 1):
     c = [55, 75, 65, 69]
     w = [4, 5, 8, 9]
+    i = [1, 2, 3, 4]
     b = [2, 2, 2, 3]
     b1 = [x - 1 for x in b]
     b2 = [x + 1 for x in b]
@@ -85,17 +187,16 @@ def secondTask(task = 1):
     if task == 2:
         print("Y ", "F1 ", "                   F2 ", "                    F3 ")
         for Y in range(Y_start, Y_end + 1):
-            result1 = F(c, w, Y, [], b)
-            result2 = F(c, w, Y, [], b1)
-            result3 = F(c, w, Y, [], b2)
+            result1 = F(c, w, Y,i, [], b)
+            result2 = F(c, w, Y,i, [], b1)
+            result3 = F(c, w, Y,i, [], b2)
 
             print(Y, result1, result2, result3)
 
     elif task == 1:
         print("Y", "F", "Список СрЗИ")
         for Y in range(Y_start, Y_end + 1):
-            result = F(c, w, Y, [], b)
-            result[1][0] = 2
+            result = F(c, w, Y,i, [], b)
             print(Y, *result)
 
 
@@ -104,22 +205,24 @@ def thirdTask(task = 1):
     c = [55, 75, 65, 69]
     w = [4, 5, 8, 9]
     a = [2, 1, 1, 1]
+    i = [1, 2, 3, 4]
     a1 = [x + 1 for x in a]
     b = [4, 2, 2, 3]
-    Y_start = 122
-    Y_end = 144
+    Y_start = 61
+    Y_end = 72
 
     if task == 2:
         print("Y  ", "F1 ", "                   F2 ")
         for Y in range(Y_start, Y_end + 1):
-            result1 = F(c, w, Y, a, b)
-            result2 = F(c, w, Y, a1, b)
+            result1 = F(c, w, Y,i, a, b)
+            result2 = F(c, w, Y,i, a1, b)
 
-            print(Y, result1[0], result2[0])
+            # print(Y, result1[0], result2[0])
+            print(Y, result1, result2)
     elif task == 1:
         print("Y", "F", "Список СрЗИ")
         for Y in range(Y_start, Y_end + 1):
-            result = F(c, w, Y, a, b)
+            result = F(c, w, Y,i, a, b)
             print(Y, result[0], result[1])
 
 
--- optimization-methods/firstLabPython/main.py
+++ optimization-methods/firstLabPython/main.py
@@ -1,140 +1,42 @@
-from math import gcd
-from functools import reduce
+def find_most_efficient_resources(F, resources):
+    resources = sorted(resources, key=lambda x: x[2] / x[1], reverse=True)  # Сортируем по эффективности
+    result = []
+    remaining_cost = F
 
-INF = 2 * 10**9
+    for res in resources:
+        count = 0
+        while remaining_cost >= res[2]:
+            count += 1
+            remaining_cost -= res[2]
+        result.append([res[0], count])
 
+    result.sort()
+    secondResult = []
 
-def F(c, w, Y, a=[], b=[]):
-    if a == []:
-        a = [0] * len(c)
-    if b == []:
-        b = [INF] * len(c)
+    for r in result:
+        secondResult.append(r[1])
+    return secondResult
 
-    step = reduce(gcd, w)
-    f = [0] * len(c)
-    szi_list = [0] * len(c)
+# Пример использования функции
+F = 100
+resources_matrix = [
+    [1, 6, 22],
+    [2, 7, 26],
+    [3, 8, 28],
+    [4, 10, 33]
+]
 
-    for i in range(len(c)):
-        max_val = min(Y // w[i], b[i])
+# 21 78 [0, 3, 0, 0]
+result = find_most_efficient_resources(F, resources_matrix)
+all_resources = set(res[0] for res in resources_matrix)
 
-        f[i] = [0] * (Y // step + 1)
-        szi_list[i] = [0] * (Y // step + 1)
-
-        if i == 0:
-            for j in range(Y // step + 1):
-                cur_f = 0
-                cur_val = max(0, a[i])
-                opt_val = [cur_val]
-                while (w[i] * cur_val <= j * step) and (cur_val <= max_val):
-                    cur_f = max(cur_f, c[i] * cur_val)
-                    if cur_f < c[i] * cur_val:
-                        cur_f = c[i] * cur_val
-                        opt_val = [cur_val]
-                    cur_val += 1
-                f[i][j] = cur_f
-                szi_list[i][j] = opt_val
-
-
-        if i > 0:
-            for j in range(Y // step + 1):
-                cur_f = 0
-                cur_val = max(0, a[i])
-                opt_val = [cur_val]
-                surplus = j * step - w[i] * cur_val
-
-                while (surplus >= 0) and (cur_val <= max_val):
-                    if cur_f < c[i] * cur_val + f[i-1][surplus // step]:
-                        cur_f = c[i] * cur_val + f[i-1][surplus // step]
-                        opt_val = szi_list[i-1][surplus // step] + [cur_val]
-                    cur_val += 1
-                    surplus = j * step - w[i] * cur_val
-                f[i][j] = cur_f
-                szi_list[i][j] = opt_val
-
-    return [f[len(c) - 1][-1], szi_list[len(c) - 1][-1]]
-
-#первая задача
-def first(task = 1):
-    c = [10, 28, 33, 35]
-    w = [6, 7, 9, 10]
-    w1 = [x - 1 for x in w]
-    w2 = [x - 2 for x in w]
-    Y_start = 24
-    Y_end = 48
-
-    if task == 2:
-        print("Y ", "F1 ", "                    F2 ", "                     F3 ")
-        for Y in range(Y_start, Y_end + 1):
-            print(Y, F(c, w, Y, a=[], b=[])[0], F(c, w1, Y, a=[], b=[])[0], F(c, w2, Y, a=[], b=[])[0])
-    elif task == 1:
-        print("Y", "F", "Список СрЗИ")
-        for Y in range(Y_start, Y_end + 1):
-            print(Y, *F(c, w, Y, a=[], b=[]))
-
-
-#вторая задача
-def second(task = 1):
-    c = [24, 21, 27, 33]
-    w = [3, 4, 6, 9]
-    b = [2, 2, 3, 3]
-    b1 = [x - 1 for x in b]
-    b2 = [x + 1 for x in b]
-    Y_start = 23
-    Y_end = 46
-
-    if task == 2:
-        print("Y ", "F1 ", "                   F2 ", "                    F3 ")
-        for Y in range(Y_start, Y_end + 1):
-            result1 = F(c, w, Y, [], b)
-            result2 = F(c, w, Y, [], b1)
-            result3 = F(c, w, Y, [], b2)
-
-            print(Y, result1, result2, result3)
-
-    elif task == 1:
-        print("Y", "F", "Список СрЗИ")
-        for Y in range(Y_start, Y_end + 1):
-            result = F(c, w, Y, [], b)
-            result[1][0] = 2
-            print(Y, *result)
-
-
-#третья задача
-def third(task = 1):
-    c = [24, 21, 27, 33]
-    w = [3, 4, 6, 9]
-    a = [1, 1, 2, 1]
-    a1 = [x + 1 for x in a]
-    b = [2, 2, 3, 3]
-    Y_start = 63
-    Y_end = 77
-
-    if task == 2:
-        print("Y  ", "F1 ", "                   F2 ")
-        for Y in range(Y_start, Y_end + 1):
-            result1 = F(c, w, Y, a, b)
-            result2 = F(c, w, Y, a1, b)
-
-            print(Y, result1[0], result2[0])
-    elif task == 1:
-        print("Y", "F", "Список СрЗИ")
-        for Y in range(Y_start, Y_end + 1):
-            result = F(c, w, Y, a, b)
-            print(Y, result[0], result[1])
-
-
-def main():
-    first(1)
-    print('\n')
-    first(2)
-    print('\nВторая задача')
-    second(1)
-    print('\n')
-    second(2)
-    print('\n Третья задача')
-    third(1)
-    print('\n')
-    third(2)
-
-if __name__ == '__main__':
-    main()
+print(result)
+# for res in all_resources:
+#     found = False
+#     for row in result:
+#         if row[0] == res:
+#             print(f"[{row[0]}, {row[1]}]")
+#             found = True
+#             break
+#     if not found:
+#         print(f"[{res}, 0]")
